40d1471e904b417c07c5268f7913f00d9b275049
kimjungjin <jungks9351@gmail.com>
2022-09-06 16:25:51 +0900

    docs : JS 알고리즘 해시테이블

--- Algorithm-JS/HashTable/신고 결과 받기.js
+++ Algorithm-JS/HashTable/신고 결과 받기.js
@@ -42,22 +42,6 @@ function solution(id_list, report, k) {
 
 // 다른 사람 풀이
 
-1;
-2;
-3;
-4;
-5;
-6;
-7;
-8;
-9;
-10;
-11;
-12;
-13;
-14;
-15;
-16;
 function solution2(id_list, report, k) {
   let reports = [...new Set(report)].map(a => {
     return a.split(' ');
